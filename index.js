bbdd0d4f04bb9f05c91b117c72329161a07f7ca2
farrukhbek26 <FSAKHATOV@GMAIL.COM>
2024-09-20 21:04:39 +0500

    Update

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { Telegraf } = require('telegraf');
 const translate = require('translation-google');
 
 const bot = new Telegraf("7013130426:AAHsGuqyL2_AMUIs_Ija0Oa5awv-xfTV1Cg");
+let count = 0;
 
 bot.start((ctx) => {
    ctx.reply(`Salom ${ctx.from.first_name}. Men Ingliz tilidan O'zbek tiliga tarjima qilib beruvchi botman.`);
+   if(ctx.from.id) count++;
 });
 
 bot.on('text', async (ctx) => {
@@ -15,9 +17,9 @@ bot.on('text', async (ctx) => {
       const translation = await translate(message, { from: "en", to: "uz" });
       ctx.reply(translation.text);  
    } catch (error) {
-    //   console.error(error);
+      console.error(error);
       ctx.reply('Xatolik yuz berdi tarjima qilishda.');
    }
 });
-
 bot.launch();
+console.log(count);
